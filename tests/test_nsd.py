b21aa98ba33e378352b05b7844a64881f2112cdd
Adar Nimrod <nimrod@shore.co.il>
2016-07-13 22:23:50 +0300

    - Flush handlers at the end of the role and wait for NSD to start listening on   port 53. - Add email alias to root. - Added some more tests.

--- tests/test_nsd.py
+++ tests/test_nsd.py
@@ -1,9 +1,46 @@
 def test_nsd(Command, Service, Ansible):
     assert Command('nsd -v').rc == 0
-    assert Service('nsd').is_running
+
+
+def test_nsd_service(Service):
+    service = Service('nsd')
+    assert service.is_running
+    try:
+        assert service.is_enabled
+    except NotImplementedError:
+        pass
+
+
+def test_nsd_config(Command, File, Ansible):
     if Ansible('setup')['ansible_facts']['ansible_os_family'] == 'OpenBSD':
         assert Command('nsd-checkconf /var/nsd/etc/nsd.conf')
     elif Ansible('setup')['ansible_facts']['ansible_os_family'] == 'Debian':
         assert Command('nsd-checkconf /etc/nsd/nsd.conf')
-        assert Service('nsd').is_enabled
+
+
+def test_nsd_directories(File, Ansible):
+    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
+    if ansible_os_family == 'OpenBSD':
+        assert File('/var/nsd/etc/nsd.conf.d').is_directory
+        assert File('/var/nsd/zones').is_directory
+    elif ansible_os_family == 'Debian':
+        assert File('/etc/nsd/nsd.conf.d').is_directory
+        assert File('/etc/nsd').is_directory
+
+
+def test_nsd_socket(Socket):
+    assert Socket('udp://0.0.0.0:53').is_listening
+
+
+def test_nsd_test_zone(Command):
     assert '127.0.0.2' in Command('dig @127.0.0.1 a.testzone').stdout
+
+
+def test_nsd_alias(File, Ansible, User):
+    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
+    if ansible_os_family == 'Debian':
+        assert User('nsd').exists
+        assert File('/etc/aliases').contains('nsd: root')
+    elif ansible_os_family == 'OpenBSD':
+        assert User('_nsd').exists
+        assert File('/etc/mail/aliases').contains('_nsd: root')
