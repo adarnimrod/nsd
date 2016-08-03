36f166971b7db3ca8f27bd9c8ac65100c76c84fc
Adar Nimrod <nimrod@shore.co.il>
2016-08-03 22:49:26 +0300

    - Added server block to nsd.conf. - Corrected names of config options under nsd_config. - Corrected tests for newer version of Molecule.

--- tests/test_nsd.py
+++ tests/test_nsd.py
@@ -1,14 +1,16 @@
-def test_nsd(Command, Service, Ansible):
-    assert Command('nsd -v').rc == 0
+def test_nsd(Command, Sudo):
+    with Sudo():
+        assert Command('nsd -v').rc == 0
 
 
-def test_nsd_service(Service):
+def test_nsd_service(Service, Sudo):
     service = Service('nsd')
-    assert service.is_running
-    try:
-        assert service.is_enabled
-    except NotImplementedError:
-        pass
+    with Sudo():
+        assert service.is_running
+        try:
+            assert service.is_enabled
+        except NotImplementedError:
+            pass
 
 
 def test_nsd_config(Command, File, Ansible):
@@ -18,11 +20,12 @@ def test_nsd_config(Command, File, Ansible):
         assert Command('nsd-checkconf /etc/nsd/nsd.conf')
 
 
-def test_nsd_directories(File, Ansible):
+def test_nsd_directories(File, Ansible, Sudo):
     ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
     if ansible_os_family == 'OpenBSD':
-        assert File('/var/nsd/etc/nsd.conf.d').is_directory
-        assert File('/var/nsd/zones').is_directory
+        with Sudo():
+            assert File('/var/nsd/etc/nsd.conf.d').is_directory
+            assert File('/var/nsd/zones').is_directory
     elif ansible_os_family == 'Debian':
         assert File('/etc/nsd/nsd.conf.d').is_directory
         assert File('/etc/nsd').is_directory
